a4c6c43f9364bff2925bd87fadf6ca642b259d73
snehapm04 <161349403+snehapm04@users.noreply.github.com>
2024-11-16 22:23:04 +0530

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -6,13 +6,20 @@ export default function SkillsPage() {
         <div>
         <label>Python</label>
           <div className="w-full bg-gray-700 rounded-full h-2.5">
-            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "80%" }}></div>
+            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "90%" }}></div>
           </div>
           <label>C</label>
           <div className="w-full bg-gray-700 rounded-full h-2.5">
             <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "80%" }}></div>
           </div>
-
+          <label>HTML</label>
+          <div className="w-full bg-gray-700 rounded-full h-2.5">
+            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "90%" }}></div>
+          </div>
+          <label>CSS</label>
+          <div className="w-full bg-gray-700 rounded-full h-2.5">
+            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "80%" }}></div>
+          </div>
           <label>JavaScript</label>
           <div className="w-full bg-gray-700 rounded-full h-2.5">
             <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "80%" }}></div>
@@ -21,4 +28,5 @@ export default function SkillsPage() {
       </div>
     );
   }
+  
   
