e99a9931db71bafeac4c8b909ec56537d4722293
snehapm04 <161349403+snehapm04@users.noreply.github.com>
2024-11-17 21:20:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <html>
-      <body className="bg-dark text-lightGray">
+      <body className="bg-dark text-lightGray min-h-screen">
         <header className="flex justify-between items-start p-4">
           {/* Name as a clickable link to return to the home page */}
           <Link href="/" passHref>
@@ -18,34 +18,34 @@ export default function Layout({ children }: LayoutProps) {
             </div>
           </Link>
           {/* Vertical menu bar on the right side */}
-          <nav className="fixed right-10 top-50 flex flex-col space-y-6 text-lg pr-4">
+          <nav className="fixed top-20 right-6 lg:top-10 lg:right-10 flex flex-col space-y-4 lg:space-y-6 text-lg pr-2 lg:pr-4">
             <Link 
               href="/skills" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
             >
               Skills
             </Link>
             <Link 
               href="/experience" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
             >
               Experience
             </Link>
             <Link 
               href="/projects" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
             >
               Projects
             </Link>
             {/*<Link 
               href="/certifications" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
             >
               Certifications
             </Link>*/}
           </nav>
         </header>
-        <main className="pt-10 pl-10">{children}</main>
+        <main className="pt-6 pl-4 lg:pt-10 lg:pl-10">{children}</main>
       </body>
     </html>
   );
