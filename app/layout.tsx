4a980780ef098ccc7764e99d430f728b912c8154
snehapm04 <161349403+snehapm04@users.noreply.github.com>
2024-11-17 21:28:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import Link from 'next/link';
 import './globals.css';
 import { ReactNode } from 'react';
@@ -8,44 +9,42 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <html>
+    <html lang="en">
+      <head>
+        {/* Include viewport meta for mobile devices */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className="bg-dark text-lightGray min-h-screen">
-        <header className="flex justify-between items-start p-4">
+        <header className="flex justify-between items-center p-4 md:p-6">
           {/* Name as a clickable link to return to the home page */}
           <Link href="/" passHref>
-            <div className="text-2xl font-bold text-cyan animate-pulse cursor-pointer">
+            <div className="text-xl md:text-2xl font-bold text-cyan animate-pulse cursor-pointer">
               I'm Sneha...
             </div>
           </Link>
           {/* Vertical menu bar on the right side */}
-          <nav className="fixed top-20 right-6 lg:top-10 lg:right-10 flex flex-col space-y-4 lg:space-y-6 text-lg pr-2 lg:pr-4">
+          <nav className="fixed top-16 right-4 lg:top-10 lg:right-10 flex flex-col space-y-3 md:space-y-4 lg:space-y-6 text-base md:text-lg pr-2 lg:pr-4">
             <Link 
               href="/skills" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
             >
               Skills
             </Link>
             <Link 
               href="/experience" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
             >
               Experience
             </Link>
             <Link 
               href="/projects" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
+              className="bg-slate hover:bg-hoverSlate transition duration-300 p-2 rounded"
             >
               Projects
             </Link>
-            {/*<Link 
-              href="/certifications" 
-              className="bg-slate hover:bg-hoverSlate transition duration-300 p-1 lg:p-2 rounded"
-            >
-              Certifications
-            </Link>*/}
           </nav>
         </header>
-        <main className="pt-6 pl-4 lg:pt-10 lg:pl-10">{children}</main>
+        <main className="pt-8 px-4 md:px-6 lg:px-10">{children}</main>
       </body>
     </html>
   );
